ef7b22ad7a1f9beaecfd98d558cb3f5f35ee1471
OkanBey11 <okangoztok@hotmail.com>
2025-08-16 14:40:58 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int myVar = 42;
+    std :: cout << "Welcome 2u. What if your age ?";
+    std :: cin >> myVar;
+    std :: cout << "Your age is : " << myVar;
+    return 0;
+}
---
+++ varbilesAndConstant.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    //varible
+    int score = 100;
+    double pi = 3.14159;
+    char initial = 'A';
+    bool is_ready = true;
+
+    std :: cout << score << " " << pi << " " << initial << " " << is_ready << "\n";
+
+    //constant
+    const int MAX_USERS = 100;
+    const double CHANGE = 3.45;
+    std :: cout << "constants: \n" << MAX_USERS << "\n" << CHANGE;
+}
